13aed2f94929fc10a6e0047760ab3f320cafd224
shankar <shankar@layerzerolabs.org>
2025-05-29 23:49:58 +0000

    feat: custom rate limit implementation
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- contracts/GMX_Adapter.sol
+++ contracts/GMX_Adapter.sol
@@ -6,6 +6,7 @@ import { MintBurnOFTAdapter } from "@layerzerolabs/oft-evm/contracts/MintBurnOFT
 import { IMintableBurnable } from "@layerzerolabs/oft-evm/contracts/interfaces/IMintableBurnable.sol";
 import { RateLimiter } from "@layerzerolabs/oapp-evm/contracts/oapp/utils/RateLimiter.sol";
 import { SendParam, MessagingFee, MessagingReceipt, OFTReceipt } from "@layerzerolabs/oft-evm/contracts/OFTCore.sol";
+import { IGMX_Adapter } from "./interfaces/IGMX_Adapter.sol";
 
 /**
  * @title MintBurnOFTAdapter Contract
@@ -17,7 +18,9 @@ import { SendParam, MessagingFee, MessagingReceipt, OFTReceipt } from "@layerzer
  * IF the 'innerToken' applies something like a transfer fee, the default will NOT work...
  * a pre/post balance check will need to be done to calculate the amountSentLD/amountReceivedLD.
  */
-contract GMX_Adapter is MintBurnOFTAdapter, RateLimiter {
+contract GMX_Adapter is MintBurnOFTAdapter, RateLimiter, IGMX_Adapter {
+    mapping(bytes32 => bool) public canOverrideRateLimit;
+
     constructor(
         RateLimitConfig[] memory _rateLimitConfigs,
         address _token,
@@ -29,11 +32,37 @@ contract GMX_Adapter is MintBurnOFTAdapter, RateLimiter {
     }
 
     /**
+     * @dev Adds an address to the list of addresses that can override the rate limit.
+     * @dev To be used for EVM chains where the address is not bytes32.
+     * @param _address The address to add to the list.
+     * @param _canOverride Whether the address can override the rate limit.
+     */
+    function modifyRateLimitOverrideList(address _address, bool _canOverride) external onlyOwner {
+        canOverrideRateLimit[_addressToBytes32(_address)] = _canOverride;
+
+        emit NewRateLimitOverrider(_addressToBytes32(_address));
+    }
+
+    /**
+     * @dev Adds an address to the list of addresses that can override the rate limit.
+     * @dev To be used for non-EVM chains where the address is bytes32.
+     * @param _address The address to add to the list.
+     * @param _canOverride Whether the address can override the rate limit.
+     */
+    function modifyRateLimitOverrideList(bytes32 _address, bool _canOverride) external onlyOwner {
+        canOverrideRateLimit[_address] = _canOverride;
+
+        emit NewRateLimitOverrider(_address);
+    }
+
+    /**
      * @dev Sets the rate limits based on RateLimitConfig array. Only callable by the owner or the rate limiter.
      * @param _rateLimitConfigs An array of RateLimitConfig structures defining the rate limits.
      */
     function setRateLimits(RateLimitConfig[] calldata _rateLimitConfigs) external onlyOwner {
         _setRateLimits(_rateLimitConfigs);
+
+        emit RateLimitUpdated(_rateLimitConfigs);
     }
 
     /**
@@ -49,7 +78,36 @@ contract GMX_Adapter is MintBurnOFTAdapter, RateLimiter {
         MessagingFee calldata _fee,
         address _refundAddress
     ) external payable override returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt) {
-        _outflow(_sendParam.dstEid, _sendParam.amountLD); // updating the rate limit per message sent
+        if (canOverrideRateLimit[bytes32(_sendParam.to)]) {
+            emit RateLimitOverrided(bytes32(_sendParam.to), _sendParam.amountLD);
+        } else {
+            _outflow(_sendParam.dstEid, _sendParam.amountLD);
+        }
         return _send(_sendParam, _fee, _refundAddress);
+    }
+
+    /**
+     * @dev Overrides the _credit function to allow for rate limit override.
+     * @param _to The address to credit.
+     * @param _amountLD The amount to credit in local decimals.
+     * @param _srcEid The source endpoint ID.
+     * @return amountReceivedLD The amount received in local decimals.
+     */
+    function _credit(
+        address _to,
+        uint256 _amountLD,
+        uint32 _srcEid
+    ) internal virtual override returns (uint256 amountReceivedLD) {
+        if (canOverrideRateLimit[_addressToBytes32(_to)]) {
+            emit RateLimitOverrided(_addressToBytes32(_to), _amountLD);
+        } else {
+            _outflow(_srcEid, _amountLD);
+        }
+
+        return super._credit(_to, _amountLD, _srcEid);
+    }
+
+    function _addressToBytes32(address _addr) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(_addr)));
     }
 }
